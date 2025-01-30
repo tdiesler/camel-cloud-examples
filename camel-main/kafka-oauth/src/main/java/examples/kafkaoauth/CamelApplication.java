37c45f4e9836965f976a898620e23c6851faa43a
Thomas Diesler <tdiesler@redhat.com>
2025-01-30 14:15:42 +0100

    RHBAC-114: OAuth Authentication for Kafka on Kubernetes

---
+++ camel-main/kafka-oauth/src/main/java/examples/kafkaoauth/CamelApplication.java
@@ -0,0 +1,12 @@
+package examples.kafkaoauth;
+
+import org.apache.camel.main.Main;
+
+public class CamelApplication {
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main(CamelApplication.class);
+        main.run(args);
+    }
+
+}
