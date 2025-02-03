a3ae8a8506dd0518bb550203bfe835edfc9e6fb6
Thomas Diesler <tdiesler@redhat.com>
2025-02-03 10:41:56 +0100

    CAMEL-21690: Fix image-push option on Kubernetes run command

---
+++ camel-main/bitcoin-source/src/main/java/examples/btcprice/CamelApplication.java
@@ -0,0 +1,12 @@
+package examples.btcprice;
+
+import org.apache.camel.main.Main;
+
+public class CamelApplication {
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main(CamelApplication.class);
+        main.run(args);
+    }
+
+}
